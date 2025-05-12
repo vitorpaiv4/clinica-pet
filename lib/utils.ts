045e872a6943ab35eb7ce4b93b09ae10064158d8
vitor paiva <paivavitorr@gmail.com>
2025-05-12 19:30:21 -0400

    foii

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export const metadata = {
+  metadataBase: new URL('https://clinica-pet.vercel.app'), // ou seu domínio
+  // ...outros metadados...
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Add the following line to install eslint-config-next as a dev dependency
+// npm install --save-dev eslint-config-next
