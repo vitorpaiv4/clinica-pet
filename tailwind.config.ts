2da57ac0ab2e56bacfba7a1d9de5b7c9988603f1
vitor paiva <paivavitorr@gmail.com>
2025-05-12 19:34:01 -0400

    soft

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
+
 import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   prefix: "",
   theme: {
